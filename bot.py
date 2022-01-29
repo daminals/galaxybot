cd0f999d6b2c54cac88929453c01c429868040ae
Daniel Kogan <danielkoganx@gmail.com>
2022-01-29 00:07:40 -0500

    so true bestie

--- bot.py
+++ bot.py
@@ -3,18 +3,18 @@
 # 01.28.2022
 
 import discord, os
+import wikipedia
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.environ.get('TOKEN', 3)
 
 bot = discord.Bot()
 from discord.ext import commands
+bot = commands.Bot(command_prefix=">")
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
-
-bot = commands.Bot(command_prefix=">")
 
 @bot.command()
 async def ping(ctx):
