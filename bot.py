3c73d253940cb2230d2887f9ad2362197bbdcd58
Daniel Kogan <danielkoganx@gmail.com>
2022-01-29 13:02:16 -0500

    autosave

--- bot.py
+++ bot.py
@@ -2,15 +2,50 @@
 # Daniel Kogan Taseen Islam Aritro Sarkar
 # 01.28.2022
 
-import discord, os
+import discord, os, json, random, requests
 import wikipedia
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.environ.get('TOKEN', 3)
 
-bot = discord.Bot()
 from discord.ext import commands
 bot = commands.Bot(command_prefix=">")
+g_rarity = []    
+
+
+class GALAXY_OBJECT:
+    def __init__(self, gdict):
+        self.name = gdict["name"]
+        self.rarity = gdict["rarity"]
+        self.wikipedia = gdict["wikipedia"]
+        self.wname = gdict['wname']
+        self.points = (1/self.rarity) * 100
+        self.img = self.get_img()        
+
+        for i in range(self.rarity):
+            g_rarity.append(self)
+
+    def get_img(self):
+        wiki = wikipedia.page(self.wname)
+        img_link = wiki.images[1]
+        print(img_link)
+        return img_link
+
+    def shorten(self, summary):
+        summary = summary.split("\n")[0]
+        return summary
+
+    def embed(self):
+        display = discord.Embed(title=self.name, description=self.shorten(wikipedia.summary(self.wname)), colour=0x87CEEB)
+        #display.set_author(name="galaxybot", icon_url=self.img)
+        display.set_image(url=self.img)
+        return display
+
+with open('galaxy.json') as galaxyjson:
+    galaxies = json.load(galaxyjson)
+
+for i in galaxies:
+    GALAXY_OBJECT(galaxies[i])
 
 @bot.event
 async def on_ready():
@@ -19,5 +54,12 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@bot.command()
+async def galaxy(ctx):
+    chosen = random.choice(g_rarity)
+    await ctx.send(embed=chosen.embed())
+    #print(g_rarity)
+    
 
 bot.run(TOKEN)
