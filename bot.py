eb03616f132b1c4155cd97f7e65fc5efba30f0bb
Daniel Kogan <danielkoganx@gmail.com>
2022-02-02 16:46:18 -0500

    pep8 style code

--- bot.py
+++ bot.py
@@ -2,17 +2,22 @@
 # Daniel Kogan Taseen Islam Aritro Sarkar
 # 01.28.2022
 
-import discord, os, json, random, requests
+from discord.ext import commands
+from dotenv import load_dotenv
+import barter
+import wikipedia
+import discord
+import os
+import json
+import random
+import requests
 import asyncio
 intents = discord.Intents.all()
-import wikipedia
-import barter
-from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.environ.get('TOKEN', 3)
 
-from discord.ext import commands
 bot = commands.Bot(command_prefix=">", intents=intents)
+
 
 class GALAXY_OBJECT:
     def __init__(self, gdict):
@@ -29,23 +34,29 @@ class GALAXY_OBJECT:
         return summary
 
     def embed(self, user):
-        display = discord.Embed(title=self.name, description=self.shorten(wikipedia.summary(self.wname)), colour=0x87CEEB)
+        display = discord.Embed(title=self.name, description=self.shorten(
+            wikipedia.summary(self.wname)), colour=0x87CEEB)
         #display.set_author(name="galaxybot", icon_url=self.img)
         display.set_image(url=self.img)
         display.add_field(name="User", value=user, inline=False)
-        display.add_field(name="Rarity", value=self.stable_rarity, inline=False)
+        display.add_field(
+            name="Rarity", value=self.stable_rarity, inline=False)
         display.add_field(name="Points", value=self.points, inline=False)
         return display
+
 
 with open('galaxy.json', 'r+') as galaxyjson:
     galaxies = json.load(galaxyjson)
 
-async def leaderboard(ctx, individual, datapoint): # leaderboard function for any desired datapoint
+
+# leaderboard function for any desired datapoint
+async def leaderboard(ctx, individual, datapoint):
     leaderboard = "```"
     data = barter.readJSON("user.json")
     if individual:
         count = data[str(individual.id)][datapoint]
-        lname = str(individual.name) + '#' + str(individual.discriminator) + ':' + str(count) + '\n'
+        lname = str(individual.name) + '#' + \
+            str(individual.discriminator) + ':' + str(count) + '\n'
         ud = await ctx.send(leaderboard + lname + '```')
         return
     server = ctx.guild
@@ -54,7 +65,8 @@ async def leaderboard(ctx, individual, datapoint): # leaderboard function for an
         discord_user = bot.get_user(int(user))
         if discord_user in server.members:
             count = data[user][datapoint]
-            lname = str(discord_user.name) + '#' + str(discord_user.discriminator) + ': ' + str(count) + '\n'
+            lname = str(discord_user.name) + '#' + \
+                str(discord_user.discriminator) + ': ' + str(count) + '\n'
             leaderboard_no_format.append([count, lname])
     leaderboard_no_format = sorted(leaderboard_no_format)
     leaderboard_no_format = leaderboard_no_format[::-1]
@@ -78,25 +90,34 @@ async def leaderboard(ctx, individual, datapoint): # leaderboard function for an
         await asyncio.sleep(1)
     """
 
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.command() # show off points
-async def points(ctx, individual: discord.Member=None):
+
+@bot.command()  # show off points
+async def points(ctx, individual: discord.Member = None):
     await leaderboard(ctx, individual, "points")
 
-@bot.command() # show off charms
-async def charms(ctx, individual: discord.Member=None):
+
+@bot.command()  # show off charms
+async def charms(ctx, individual: discord.Member = None):
     await leaderboard(ctx, individual, "charms")
+
 
 @bot.command()
 async def shop(ctx):
-    embed = discord.Embed(title="Shop", description="Galaxy Shop. Use >buy item_name to purchase items with your points! Enter 'basic', 'super', or 'galatic' to purchase your charm!", colour=0x87CEEB)
-    embed.add_field(name="Basic Charm", value="500 points, makes rarer galaxies appear more frequently for you!", inline=False)
-    embed.add_field(name="Super Charm", value="2000 points, 5 times more effective than basic charm!", inline=False)
-    embed.add_field(name="Galatic Charm", value="5000 points, 3 times more effective than the Super Charm!", inline=False)
+    embed = discord.Embed(
+        title="Shop", description="Galaxy Shop. Use >buy item_name to purchase items with your points! Enter 'basic', 'super', or 'galatic' to purchase your charm!", colour=0x87CEEB)
+    embed.add_field(name="Basic Charm",
+                    value="500 points, makes rarer galaxies appear more frequently for you!", inline=False)
+    embed.add_field(name="Super Charm",
+                    value="2000 points, 5 times more effective than basic charm!", inline=False)
+    embed.add_field(name="Galatic Charm",
+                    value="5000 points, 3 times more effective than the Super Charm!", inline=False)
     await ctx.send(embed=embed)
+
 
 @bot.command()
 async def buy(ctx, charm):
@@ -104,6 +125,7 @@ async def buy(ctx, charm):
         await ctx.send(barter.add_charm(str(ctx.author.id), charm))
     else:
         await ctx.send("that charm doesn't seem to be in the shop.")
+
 
 @bot.command()
 async def missing(ctx):
@@ -116,7 +138,7 @@ async def missing(ctx):
     for i in all:
         if all[i]['name'] not in user_disc:
             missing.append(all[i]['name'])
-            missing_string += all[i]['name'] +'\n'
+            missing_string += all[i]['name'] + '\n'
     await ctx.send(missing_string + '```')
 
 
@@ -147,6 +169,6 @@ async def galaxy(ctx):
             await ctx.send("Congrats! You have discovered every galaxy!", file=discord.File('winner.png'))
     except:
         print('new user')
-    
+
 
 bot.run(TOKEN)
